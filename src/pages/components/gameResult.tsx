eb1b25422f63f52ceb268b10922ce1ed47df9f1b
daniel-rodriguez-prft <daniel.rodriguezm@perficient.com>
2023-08-23 17:21:54 -0500

    page improvements

--- src/pages/components/gameResult.tsx
+++ src/pages/components/gameResult.tsx
@@ -8,17 +8,6 @@ export default function GameResult({game}: {game: GameResponse}) {
       <h2>
         <Link href={`/game/${game.id}`}>{game.name}</Link>
       </h2>
-      <Image
-        src={
-          game.background_image ??
-          "https://placehold.co/600x400/jpg?text=No+image"
-        }
-        placeholder='blur'
-        blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII='
-        alt='placeholder'
-        width='640'
-        height='360'
-      />
     </div>
   )
 }
--- src/pages/game/[gameId].tsx
+++ src/pages/game/[gameId].tsx
@@ -1,11 +1,7 @@
 import {GetStaticPathsContext, GetStaticPathsResult, GetStaticProps} from "next"
-import {useRouter} from "next/router"
+import Image from "next/image"
+import Link from "next/link"
 import {CORS_WORKER, APP_ENDPOINT, API_KEY} from "../constants"
-import useGameApi from "../hooks/useGameApi"
-type Repo = {
-  name: string
-  stargazers_count: number
-}
 
 export const getStaticProps: GetStaticProps<{}> = async context => {
   const {params} = context
@@ -30,11 +26,18 @@ export async function getStaticPaths(
 export default function Page({data}: {data: any}) {
   return (
     <div>
+      <Link href='/'>Regresar</Link>
       <p>{data.name}</p>
-      <p
-        dangerouslySetInnerHTML={{
-          __html: data.description
-        }}></p>
+      <div>{data.description_raw}</div>
+      <Image
+        src={
+          data.background_image ??
+          "https://placehold.co/600x400/jpg?text=No+image"
+        }
+        alt='placeholder'
+        width='640'
+        height='360'
+      />
     </div>
   )
 }
