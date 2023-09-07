553d3a70f4fcff0d7891aac3de2623a5e6d8aab3
Pablo Quijano Jaramillo <pablo.quijanoj@perficient.com>
2023-09-07 09:17:41 -0500

    Initial code

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -1,25 +1,42 @@
-import Link from "next/link"
-import {useEffect, useState} from "react"
-import {useFetch} from "../hooks/useFetch"
-import {GameResponse} from "../types"
-import Form from "./form"
-import {Loading} from "./loading"
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import Form from "./form";
+import { Loading } from "./loading";
+import { GameResponse } from "@/types";
 
 export function Game() {
-  const [searchTerm, setSearchTerm] = useState("")
-  const {asyncFn, data, status, error} = useFetch({
-    status: "idle",
-    data: null,
-    error: null
-  })
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const [status, setStatus] = useState("idle");
+  const [data, setData] = useState<GameResponse | null>(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    const promise: Promise<GameResponse> = fetch(
-      `${process.env.NEXT_PUBLIC_CORS_WORKER}/?${process.env.NEXT_PUBLIC_APP_ENDPOINT}?key=${process.env.NEXT_PUBLIC_API_KEY}&search=${searchTerm}`
-    ).then(data => data.json())
+    const fetchData = (url: string) => {
+      const promise = fetch(url).then((res) => res.json());
 
-    asyncFn(promise)
-  }, [asyncFn, searchTerm])
+      if (!promise || !promise.then) {
+        throw new Error("Error");
+      }
+      setStatus("pending");
+
+      return promise.then(
+        (data) => {
+          setData(data);
+          setStatus("success");
+          return data;
+        },
+        (error) => {
+          setError(error);
+          return Promise.reject(error);
+        }
+      );
+    };
+
+    fetchData(
+      `${process.env.NEXT_PUBLIC_CORS_WORKER}/?${process.env.NEXT_PUBLIC_APP_ENDPOINT}?key=${process.env.NEXT_PUBLIC_API_KEY}&search=${searchTerm}`
+    );
+  }, [searchTerm]);
 
   return (
     <main className="container mx-auto p-4">
@@ -28,13 +45,14 @@ export function Game() {
       {status === "pending" && <Loading />}
       {data && status === "success" && (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3">
-          {data.results.map(element => {
+          {data.results.map((element) => {
             return (
               <div key={element.id}>
                 <Link
                   className="cursor-pointer bg-slate-200 rounded-md p-4 w-full h-full block"
-                  href={`/game/${element.id}`}>
-                    {element.name}
+                  href={`/game/${element.id}`}
+                >
+                  {element.name}
                 </Link>
               </div>
             );
@@ -44,6 +62,7 @@ export function Game() {
       {data?.results.length === 0 && status !== "pending" && (
         <p>Sin resultados</p>
       )}
+      {error && <p>Hubo un error</p>}
     </main>
   );
 }
